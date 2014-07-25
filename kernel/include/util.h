c46cc6a1a637485d4cd1cc8bfd673fab82d6777d
Michael Tuttle <tuttlem@gmail.com>
2014-07-25 21:46:42 +1000

    Added extra assembly functions
    With the functions listed at http://wiki.osdev.org/Inline_Assembly/Examples#PGFLUSHTLB, the
    util.h header now has a few more functions. This won't be their final place, but will be
    distributed into different parts of the kernel as those areas are complete. See CPUID for
    an example - it's in its own module.

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -91,4 +91,63 @@ static inline void farpokeb(u16 sel, void *off, u8 v) {
        : "g"(sel), "r"(off), "r"(v));
 }
 
+/* Read the current value of the CPU's time-stamp counter */
+static inline u64 rdtsc() {
+  u64 ret;
+  asm volatile ("rdtsc" : "=A"(ret));
+  return ret;
+}
+
+/* Read the value of cr1 */
+static inline u64 get_cr1() {
+  u64 ret;
+  asm volatile ("mov %%cr1, %0" : "=r"(val));
+  return ret;
+}
+
+/* Read the value of cr2 */
+static inline u64 get_cr2() {
+  u64 ret;
+  asm volatile ("mov %%cr2, %0" : "=r"(val));
+  return ret;
+}
+
+/* Read the value of cr3 */
+static inline u64 get_cr3() {
+  u64 ret;
+  asm volatile ("mov %%cr3, %0" : "=r"(val));
+  return ret;
+}
+
+/* Read the value of cr4 */
+static inline u64 get_cr4() {
+  u64 ret;
+  asm volatile ("mov %%cr4, %0" : "=r"(val));
+  return ret;
+}
+
+/* Read the value of cr0 */
+static inline u64 get_cr0() {
+  u64 ret;
+  asm volatile ("mov %%cr0, %0" : "=r"(val));
+  return ret;
+}
+
+/* Invalidate the TLB (Translation Lookaside Buffer) for one specific virtual address */
+static inline void invlpg(void *m) {
+  asm volatile ("invlpg (%0)" : : "m"(m) : "memory");
+}
+
+/* Write a 64-bit value to a MSR */
+static inline void wrmsr(u32 id, u64 value) {
+  asm volatile ("wrmsr" : : "c"(id), "A"(value));
+}
+
+/* Read a 64-bit value from a MSR */
+static inline u64 rdmsr(u32 id) {
+  u64 value;
+  asm volatile ("rdmsr" : "=A"(value) : "c"(id));
+  return value;
+}
+
 #endif
